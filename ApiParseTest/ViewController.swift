4b10a9e92411a4eb54da631e5b32e80be1b98a6c
nakamura.takakazu <koreyanjp@yahoo.co.jp>
2014-09-21 10:22:59 +0900

    Initial Commit

---
+++ ApiParseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApiParseTest
+//
+//  Created by 中村 貴一 on 2014/09/21.
+//  Copyright (c) 2014年 Keech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
